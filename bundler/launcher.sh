82316549e0b7c94d10eed499fe5fe180a795b912
Richard Hult <richard@imendio.com>
2008-02-09 15:21:02 +0100

    Add support for running the app with gdb and to add extra arguments

--- bundler/launcher.sh
+++ bundler/launcher.sh
@@ -2,6 +2,9 @@
 
 if test "x$IGE_MAC_APP_DEBUG" != x; then
     set -x
+    EXEC="gdb --args"
+else
+    EXEC=exec
 fi
 
 name="`basename $0`"
@@ -31,10 +34,14 @@ export PANGO_RC_FILE="$bundle_etc/pango/pangorc"
 # We need a UTF-8 locale.
 export LANG="`grep \`defaults read .GlobalPreferences AppleCollationOrder 2>&1\`_ /usr/share/locale/locale.alias  | tail -n1 | sed 's/\./ /' | awk '{print $2}'`.UTF-8"
 
-export CHARSETALIASDIR="$bundle_lib"
+if test -f "$bundle_lib/charset.alias"; then
+    export CHARSETALIASDIR="$bundle_lib"
+fi
 
-if test -f "$bundle_contents/environment.sh"; then
-  source "$bundle_contents/environment.sh"
+# Extra arguments can be added in environment.sh.
+EXTRA_ARGS=
+if test -f "$bundle_res/environment.sh"; then
+  source "$bundle_res/environment.sh"
 fi
 
 # Strip out the argument added by the OS.
@@ -42,4 +49,4 @@ if [ x`echo "x$1" | sed -e "s/^x-psn_.*//"` == x ]; then
     shift 1
 fi
 
-exec "$bundle_contents/MacOS/$name-bin" $*
+$EXEC "$bundle_contents/MacOS/$name-bin" $* $EXTRA_ARGS
