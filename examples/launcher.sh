0c7464940013e450a91caf611cc2bd3724dfcdcb
John Ralls <jralls@ceridwen.us>
2014-12-30 14:44:45 -0800

    Update GDK_PIXBUF_MODULE_FILE value to reflect new path from separated gdk-pixbuf.

--- examples/launcher.sh
+++ examples/launcher.sh
@@ -31,7 +31,11 @@ export GTK_PATH="$bundle_res"
 
 export GTK2_RC_FILES="$bundle_etc/gtk-2.0/gtkrc"
 export GTK_IM_MODULE_FILE="$bundle_etc/gtk-2.0/gtk.immodules"
-export GDK_PIXBUF_MODULE_FILE="$bundle_etc/gtk-2.0/gdk-pixbuf.loaders"
+#N.B. When gdk-pixbuf was separated from Gtk+ the location of the
+#loaders cache changed as well. Depending on the version of Gtk+ that
+#you built with you may still need to use the old location:
+#export GDK_PIXBUF_MODULE_FILE="$bundle_etc/gtk-2.0/gdk-pixbuf.loaders"
+export GDK_PIXBUF_MODULE_FILE="$bundle_lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
 export PANGO_LIBDIR="$bundle_lib"
 export PANGO_SYSCONFDIR="$bundle_etc"
 
